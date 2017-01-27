38736ec8dee34baf5ada4123b492c8951b145d9d
Sauvik Dolui <sauvik.dolui@innofied.com>
2017-01-28 02:34:00 +0530

    Initial Commit

---
+++ PinterestHomeTests/PinterestHomeTests.swift
@@ -0,0 +1,36 @@
+//
+//  PinterestHomeTests.swift
+//  PinterestHomeTests
+//
+//  Created by Sauvik Dolui on 28/01/17.
+//  Copyright © 2017 Sauvik Dolui. All rights reserved.
+//
+
+import XCTest
+@testable import PinterestHome
+
+class PinterestHomeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
